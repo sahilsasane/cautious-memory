9c1c5bb3ea9bc85e01a4dd6b516c4fd24c2afdbd
sahil <shsasane22@gmail.com>
2025-02-22 14:19:50 +0530

    Add error handling for bad requests and improve JSON decoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,16 +10,15 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest,
-			err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
