0a9e6192a623309a8b883a3abc8a28658b8d5b28
sahil <shsasane22@gmail.com>
2025-02-25 21:43:22 +0530

    Add database configuration and validation for movie creation

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module greenlight.sahil.net
 go 1.23.2
 
 require github.com/julienschmidt/httprouter v1.3.0 // direct
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -12,16 +12,15 @@ var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 type Runtime int32
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
-	jsonValue := fmt.Sprintf("%d min", r)
+	jsonValue := fmt.Sprintf("%d mins", r)
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
 }
 
-func (r *Runtime) UnMarshallJSON(jsonValue []byte) error {
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
 	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
-
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
