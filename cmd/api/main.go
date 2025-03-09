7c9bb0e8c50e473f7f853606609e44a3813591c9
sahil <shsasane22@gmail.com>
2025-03-09 22:28:25 +0530

    Add CORS configuration with trusted origins handling in middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -74,6 +78,10 @@ func main() {
 
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "02570437a73b2f", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight<no-reply@greenlight.sahil.net>", "SMTP sender")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
